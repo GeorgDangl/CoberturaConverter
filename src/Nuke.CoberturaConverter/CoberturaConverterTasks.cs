2cec6633cc025ab3c021f417d25c64e3a2707a88
Georg Dangl <georg@dangl.me>
2018-03-01 23:40:13 +0100

    Add Nuke.CoberturaConverter

---
+++ src/Nuke.CoberturaConverter/CoberturaConverterTasks.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using static CoberturaConverter.Core.CoberturaConverter;
+
+namespace Nuke.CoberturaConverter
+{
+    public class CoberturaConverterTasks
+    {
+        public static async Task OpenCoverToCobertura(OpenCoverConversionSettings settings)
+        {
+            using (var inputStream = File.OpenRead(settings.InputFile))
+            {
+                using (var coberturaStream = ConvertOpenCoverToCobertura(inputStream))
+                {
+                    using (var outputStream = File.Create(settings.OutputFile))
+                    {
+                        await coberturaStream.CopyToAsync(outputStream);
+                    }
+                }
+            }
+        }
+
+        public static async Task DotCoverToCobertura(DotCoverConversionSettings settings)
+        {
+            using (var inputStream = File.OpenRead(settings.InputFile))
+            {
+                using (var coberturaStream = ConvertDotCoverToCobertura(inputStream))
+                {
+                    using (var outputStream = File.Create(settings.OutputFile))
+                    {
+                        await coberturaStream.CopyToAsync(outputStream);
+                    }
+                }
+            }
+        }
+    }
+}
