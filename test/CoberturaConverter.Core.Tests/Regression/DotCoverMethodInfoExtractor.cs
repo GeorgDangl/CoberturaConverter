0fa11058185e4ca4b44bef05d7e504614a4823b3
Georg Dangl <dangl@iabi.eu>
2018-05-28 10:01:51 +0200

    Add regression test, see issue #1

---
+++ test/CoberturaConverter.Core.Tests/Regression/DotCoverMethodInfoExtractor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CoberturaConverter.Core.DotCover;
+using Xunit;
+
+namespace CoberturaConverter.Core.Tests.Regression
+{
+    public class DotCoverMethodInfoExtractor
+    {
+        [Fact]
+        public void CanRead()
+        {
+            using (var xmlStream = TestFilesFactory.GetTestFileStream(DotCoverConverter.Core.Tests.TestFile.RegressionDotCoverMethodInfoExtractor))
+            {
+                var parser = new DotCoverParser(xmlStream);
+                var dotCoverReport = parser.ParseDotCoverReport();
+                Assert.NotNull(dotCoverReport);
+                Assert.NotEmpty(dotCoverReport.Assemblies);
+            }
+        }
+    }
+}
